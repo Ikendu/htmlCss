bf2d1056dc675b03d47dfdcfbc30871ab55374c6
Aniede Chibundu David <ndubest56@gmail.com>
2023-06-02 07:21:03 +0100

    added header page

---
+++ gift shop/git commit/header.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-expand-lg" id="navbar">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#"><img src="image/logo.jpg" width="50px" id="logo"><span class="name">Lifella</span>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation" id="button">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse " id="navbarTogglerDemo02">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0 " id="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#front-page">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="#about-us">About us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="#products">Products</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="#contact-us">Contact us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="#social">Social Media</a>
+            </li>
+           
+          </ul>
+          <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+</nav>
+  
