97923d2ae88d0de17ed6c4673dadac08a18b6b15
Aniede Chibundu David <ndubest56@gmail.com>
2023-06-02 07:19:55 +0100

    added footer page

---
+++ gift shop/git commit/footer.php
@@ -0,0 +1,22 @@
+ <footer>
+    <a href="#contact-us"><p>Contact us</p></a>
+    <div> 
+      <p><i class="icon-copyright"></i> Copyright 2023 © Lifella hairs. All rights reserved</p>
+      <p>Privacy Policies</p>
+    </div>
+ </footer>
+
+ <style type="text/css">
+   footer{
+  width: 100%;
+  height: 150px;
+  background-color: #0E0A68;
+  text-align: center;
+  padding: 20px;
+  line-height: 30px;
+  color: #AEBDAD;
+  position: relative;
+  bottom: -20px;
+}
+
+ </style>
